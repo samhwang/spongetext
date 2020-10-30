9337c0328401073745c0fc0f011b5ddd11dc7324
Sam Huynh <samhwang2112.dev@gmail.com>
2020-10-30 13:50:52 +1100

    Add tests

---
+++ src/spongify.test.ts
@@ -0,0 +1,19 @@
+import faker from 'faker';
+import spongify from './spongify';
+
+describe('Spongify test', () => {
+  const input = faker.lorem.sentence(15);
+  const output = spongify(input);
+
+  it('should spongify text properly', () => {
+    expect(output).toBeDefined();
+  });
+
+  it('should generate a string output', () => {
+    expect(typeof output).toBe('string');
+  });
+
+  it('should have the same length as the input', () => {
+    expect(output.length).toEqual(input.length);
+  });
+});
