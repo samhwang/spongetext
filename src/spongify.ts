87baf356e87ac7c44267624cccd7b0025fa5b456
Sam Huynh <samhwang2112.dev@gmail.com>
2020-10-30 13:24:17 +1100

    Spongify text at random intervals

--- src/spongify.ts
+++ src/spongify.ts
@@ -4,10 +4,27 @@
  * @returns {string} spongified sentence
  */
 function spongify(sentence: string): string {
-  const lowercaseSentence = sentence.toLowerCase();
+  return sentence
+    .toLowerCase()
+    .split('')
+    .reduce((outputText, character): string => {
+      const randomBoolean = getRandomBoolean();
+      const spongeCharacter = randomBoolean
+        ? character.toUpperCase()
+        : character;
 
-  const output = sentence;
-  return sentence;
+      return `${outputText}${spongeCharacter}`;
+    }, '');
+}
+
+/**
+ * Get a random boolean, used to determine
+ * if character should be uppercase or not.
+ * @returns {boolean}
+ */
+function getRandomBoolean(): boolean {
+  const randomBinaryNumber = Math.floor(Math.random() * Math.floor(2));
+  return Boolean(randomBinaryNumber);
 }
 
 export default spongify;
